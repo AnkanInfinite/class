37958dc8164dbaa36f771ea31413aecbd540084e
AnkanInfinite <ankankundu907@gmail.com>
2025-10-26 13:47:41 +0530

    done

---
+++ heart.c
@@ -0,0 +1,20 @@
+//This is To print a Heart Shape using C Programming Language
+/*
+
+ **   **
+**** ****
+*********
+ *******
+  *****
+   ***
+    *
+*/
+#include <stdio.h>
+#define width 9
+int main(){
+	int i,j,k;
+	for(i=0;i<width-2,i++){
+		
+    }    
+
+}
