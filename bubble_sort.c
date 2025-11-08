536444ab6aef2f245556bed1e37a75b77cca7fdc
AnkanInfinite <ankankundu907@gmail.com>
2025-11-08 12:13:19 +0530

    initial commit

---
+++ bubble_sort.c
@@ -0,0 +1,33 @@
+//Implementing bubble sort algorithm to sort an array in ascending order
+#include<stdio.h>
+void bubbleSort(int array[],int size);
+int main(){
+    int arr[]={23,43,23,7,3465,6,74,7,345,73,45,54,9};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    printf("Existing array : \n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    bubbleSort(arr,n);
+    printf("Array after implementing Bubble sort : \n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    return 0;
+
+}
+
+//Defining Bubble sort
+void bubbleSort(int array[],int size){
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-i-1;j++){
+            if(array[j]>array[j+1]){
+                int temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+            }
+        }
+    }
+}
