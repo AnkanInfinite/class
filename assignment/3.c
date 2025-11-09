0a3207d4527c4171b2d4be70350fbb846511038f
AnkanInfinite <ankankundu907@gmail.com>
2025-11-09 21:40:50 +0530

    added more comments and made more general by calculating last element by length of  array

--- assignment/3.c
+++ assignment/3.c
@@ -7,9 +7,9 @@ using only pointer arithmetic.
 int main(){
     int A[5] = {10, 20, 30, 40, 50};
     int length=5;//The number of elements in array
-    int *ptr=A;
+    int *ptr=A;//Stores address of 1st element of array
     printf("The Second element of the array is %d \n",*(ptr + 1));
-    printf("The last element of the array is %d \n",*(ptr + 4));
+    printf("The last element of the array is %d \n",*(ptr + length-1));
     printf("The middle element of the array is %d \n",*(ptr + (length/2)));
     return 0;
 }
